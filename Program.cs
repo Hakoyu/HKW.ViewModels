a73699ae75ec26821b7b11973a8c88a3b3c4a712
Hakoyu <Hakoyu@foxmail.com>
2023-05-17 20:53:40 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿#if DEBUG
+using System;
+
+#endif
+
+namespace HKW.ViewModels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+#if DEBUG
+        Console.WriteLine("Hello, World!");
+#endif
+    }
+}
---
+++ Src/Controls/ListBoxVM.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace HKW.ViewModels.Controls;
+
+/// <summary>
+/// 列表模型,用于MVVM
+/// </summary>
+[DebuggerDisplay("{Name},Count = {ItemsSource.Count}")]
+public partial class ListBoxVM : SelectorVM<ListBoxItemVM>
+{
+    /// <summary>
+    /// 构造
+    /// </summary>
+    public ListBoxVM()
+    {
+        ItemsSource ??= new();
+    }
+}
